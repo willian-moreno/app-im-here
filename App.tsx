9c393c57ff3e50b163c54a46319663f4a8e0034c
Willian Alves Moreno <willianam@uninove.br>
2025-02-24 19:03:00 -0300

    feat: Utilizando e estilizando statusbar

--- App.tsx
+++ App.tsx
@@ -1,5 +1,11 @@
+import { StatusBar } from 'react-native'
 import { Home } from './src/screens/home'
 
 export function App() {
-  return <Home />
+  return (
+    <>
+      <StatusBar barStyle={'light-content'} backgroundColor={'transparent'} translucent />
+      <Home />
+    </>
+  )
 }
